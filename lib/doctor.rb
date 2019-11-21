c1896eb462e0ba9901f6a842b103d54082bb2858
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-21 02:05:48 +0000

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -7,21 +7,21 @@ class Doctor
     @name = name
     @@all << self
   end
-
-  def appointments
-    Appointment.all.select {|appointment| appointment.doctor == self}
-  end
-
-  def patients
-    appointments.map {|appointment| appointment.patient}
-  end
-
+  
   def self.all
     @@all
   end
 
-  def new_appointment(patient, date)
-    Appointment.new(patient, date, self)
+  def appointments
+    Appointment.all.find_all {|appointment| appointment.doctor == self}
+  end
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
   end
 
+  def patients
+    appointments.each do |appointment| puts appointment.patient
+    end
+  end
 end 
