3c7de926c1021d84bae9230a0aab18e5c760f226
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-11-20 23:03:48 -0500

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,25 @@
+require 'pry'
+class Doctor
+  attr_reader :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+
+  def appointments
+    Appointment.all.select { |a| a.doctor == self }
+  end
+
+  def patients
+    self.appointments.collect { |a| a.patient }
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def self.all
+    @@all
+  end
+end
