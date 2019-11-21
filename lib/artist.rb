3785162d7f6235a2e08011f90b2ed0fca03ff269
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-21 01:38:55 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+require "pry"
+
+class Artist 
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def songs 
+    # Song.all.select do |song|
+    #   song.artist == self
+    Song.all.select {|song| song.artist == self}
+  # end
+  end
+  
+  def genres
+    # self.songs.collect do |song|
+    #   song.genre
+    # end
+    songs.map {|song| song.genre}
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_song(song, genre)
+    Song.new(name, self, genre)
+  end 
+  
+end 
