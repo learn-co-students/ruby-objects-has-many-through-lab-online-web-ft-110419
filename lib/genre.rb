4a955e8da6f57608a9abfcb666125349265b1028
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-23 20:11:50 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,14 @@
+class Genre
+  attr_reader :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def songs
+    Song.all
+  end
+end
